7b96741027bd88bdae0215f5b146e92709f160ea
shuhan <438115215@qq.com>
2022-06-22 11:30:02 +0800

    ok
    ok

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <HelloWorld msg="Welcome to Your 龚仁洲 page" />
   </div>
 </template>
 
